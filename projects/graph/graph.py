8b87c09eb8455d59d51cfb5c012df5f81dc3d6f2
jvgiv <jvgiv12@gmail.com>
2019-09-09 18:45:32 -0400

    Some changes added

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -1,6 +1,9 @@
 """
 Simple graph implementation
 """
+
+# diff bt Breadth first and depth first
+    # depth goes down, breadth goes across
 from util import Stack, Queue  # These may come in handy
 
 class Graph:
@@ -11,24 +14,60 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex] = set()
+        
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 and v2 in self.vertices:
+            self.vertices[v1].add[v2]
+        else:
+            print("ERROR: VERTEX DOESNT EXIST")
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        # steps to complete traversal
+
+        # create an empty queue using the provided queue
+        q = Queue()
+        # create set to store nodes that we have visited
+        visited = set()
+        # add starting_vertex to the queue
+        q.enqueue(starting_vertex)
+
+    
+
+        # while loop - while queue is not empty
+        # iterate through each level starting at first vert
+        while q.size() > 0:
+        ### dequeue first vertex in queue
+            current = q.dequeue()
+        ### if it hasnt been visited yet, visit it! add it to the set
+            if current not in visited:
+                print(current)
+                visited.add(current)
+        # grab next, add it to the queue
+                for next_vert in self.vertices[current]:
+                    q.enqueue(next_vert)
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        s = Stack()
+        visited = set()
+        s.push(starting_vertex)
+
+        while s.size() > 0:
+            current = s.pop()
+            if current not in visted:
+                print(current)
+                visited.add(current)
+                for next_vert in self.vertices[current]:
+                    s.push(next_vert)
     def dft_recursive(self, starting_vertex):
         """
         Print each vertex in depth-first order
